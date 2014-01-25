32f9fca837922166acb1c965824ad5edee3d8266
gnuhub <gnuhub@gmail.com>
2014-01-25 22:52:37 +0800

    gnuhub add multi-term and delete hight light line

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
-(global-hl-line-mode 1)
+;;(global-hl-line-mode 1)
 (global-linum-mode t)
 (display-time-mode 1)
+
+;; http://www.emacswiki.org/emacs/MultiTerm
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'multi-term)
+(setq multi-term-program "/bin/zsh")
