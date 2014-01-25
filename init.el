807e08aa2cfce2386eb22f32a71140f86e1659f0
gnuhub <gnuhub@gmail.com>
2014-01-25 23:33:41 +0800

    config helm

--- init.el
+++ init.el
@@ -2,9 +2,26 @@
 (global-linum-mode t)
 (display-time-mode 1)
 
-;; http://www.emacswiki.org/emacs/MultiTerm
+;; add lisp to load-path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+;; add subdirs to load-path
+(defun add-subdirs-to-load-path (dir)
+  "Recursive add directories to `load-path'."
+  (let ((default-directory (file-name-as-directory dir)))
+    (add-to-list 'load-path dir)
+    (normal-top-level-add-subdirs-to-load-path)))
+
+;; add the subdirs of site-list to load-path
+(add-subdirs-to-load-path (expand-file-name "site-lisp" user-emacs-directory))
+
+;; https://github.com/emacs-helm/helm
+(require 'helm-match-plugin)
+(require 'helm-config)       
+(helm-mode 1)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+;; http://www.emacswiki.org/emacs/MultiTerm
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
 
@@ -13,3 +30,4 @@
 (require 'window-numbering)
 (window-numbering-mode 1)
 (winner-mode 1)
+
