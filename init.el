2766f9998b663fe47b3821f812c0d93f2f82572f
gnuhub <gnuhub@gmail.com>
2014-01-25 23:03:08 +0800

    gnuhub add windows-numbering

--- init.el
+++ init.el
@@ -4,5 +4,12 @@
 
 ;; http://www.emacswiki.org/emacs/MultiTerm
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
+
+;; http://blog.binchen.org/?p=375
+;; https://github.com/nschum/window-numbering.el
+(require 'window-numbering)
+(window-numbering-mode 1)
+(winner-mode 1)
